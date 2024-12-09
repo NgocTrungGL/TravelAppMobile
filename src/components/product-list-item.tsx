08db70ea99e4abed9f573d6ea74a0f373df2d7ec
NguyenDucMinhKhoi <123210036@sv1.dut.udn.vn>
2024-12-09 10:32:51 +0700

    add css

--- src/components/product-list-item.tsx
+++ src/components/product-list-item.tsx
@@ -28,35 +28,35 @@ export const ProductListItem = ({
     );
 };
 
-// const styles = StyleSheet.create({
-//     item: {
-//         width: "48%",
-//         backgroundColor: "white",
-//         marginVertical: 8,
-//         borderRadius: 10,
-//         overflow: "hidden",
-//     },
-//     itemImageContainer: {
-//         borderRadius: 10,
-//         width: "100%",
-//         height: 150,
-//     },
-//     itemImage: {
-//         width: "100%",
-//         height: "100%",
-//         resizeMode: "cover",
-//     },
-//     itemTextContainer: {
-//         padding: 8,
-//         alignItems: "flex-start",
-//         gap: 4,
-//     },
-//     itemTitle: {
-//         fontSize: 16,
-//         color: "#888",
-//     },
-//     itemPrice: {
-//         fontSize: 14,
-//         fontWeight: "bold",
-//     },
-// });
+const styles = StyleSheet.create({
+    item: {
+        width: "48%",
+        backgroundColor: "white",
+        marginVertical: 8,
+        borderRadius: 10,
+        overflow: "hidden",
+    },
+    itemImageContainer: {
+        borderRadius: 10,
+        width: "100%",
+        height: 150,
+    },
+    itemImage: {
+        width: "100%",
+        height: "100%",
+        resizeMode: "cover",
+    },
+    itemTextContainer: {
+        padding: 8,
+        alignItems: "flex-start",
+        gap: 4,
+    },
+    itemTitle: {
+        fontSize: 16,
+        color: "#888",
+    },
+    itemPrice: {
+        fontSize: 14,
+        fontWeight: "bold",
+    },
+});
