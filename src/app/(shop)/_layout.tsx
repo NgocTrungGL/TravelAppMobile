23a8af8bbc2e9001c67cda9612f51a6bcba3cb09
NguyenDucMinhKhoi <123210036@sv1.dut.udn.vn>
2024-12-09 10:38:15 +0700

    add page

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -4,60 +4,60 @@ import { ActivityIndicator, StyleSheet } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import { useAuth } from "../../providers/auth-provider";
 
-// function TabBarIcon(props: {
-//     name: React.ComponentProps<typeof FontAwesome>["name"];
-//     color: string;
-// }) {
-//     return <FontAwesome size={24} {...props} style={{ color: "#1BC464" }} />;
-// }
+function TabBarIcon(props: {
+    name: React.ComponentProps<typeof FontAwesome>["name"];
+    color: string;
+}) {
+    return <FontAwesome size={24} {...props} style={{ color: "#1BC464" }} />;
+}
 
-// const TabsLayout = () => {
-//     const { session, mounting } = useAuth();
+const TabsLayout = () => {
+    const { session, mounting } = useAuth();
 
-//     if (mounting) return <ActivityIndicator />;
-//     if (!session) return <Redirect href="/auth" />;
+    if (mounting) return <ActivityIndicator />;
+    if (!session) return <Redirect href="/auth" />;
 
-//     return (
-//         <SafeAreaView edges={["top"]} style={styles.safeArea}>
-//             <Tabs
-//                 screenOptions={{
-//                     tabBarActiveTintColor: "#1BC464",
-//                     tabBarInactiveTintColor: "gray",
-//                     tabBarLabelStyle: { fontSize: 16 },
-//                     tabBarStyle: {
-//                         borderTopLeftRadius: 20,
-//                         borderTopRightRadius: 20,
-//                         paddingTop: 10,
-//                     },
-//                     headerShown: false,
-//                 }}
-//             >
-//                 <Tabs.Screen
-//                     name="index"
-//                     options={{
-//                         title: "shop",
-//                         tabBarIcon(props) {
-//                             return (
-//                                 <TabBarIcon {...props} name="shopping-cart" />
-//                             );
-//                         },
-//                     }}
-//                 />
-//                 <Tabs.Screen
-//                     name="orders"
-//                     options={{
-//                         title: "Orders",
-//                         tabBarIcon(props) {
-//                             return <TabBarIcon {...props} name="book" />;
-//                         },
-//                     }}
-//                 />
-//             </Tabs>
-//         </SafeAreaView>
-//     );
-// };
+    return (
+        <SafeAreaView edges={["top"]} style={styles.safeArea}>
+            <Tabs
+                screenOptions={{
+                    tabBarActiveTintColor: "#1BC464",
+                    tabBarInactiveTintColor: "gray",
+                    tabBarLabelStyle: { fontSize: 16 },
+                    tabBarStyle: {
+                        borderTopLeftRadius: 20,
+                        borderTopRightRadius: 20,
+                        paddingTop: 10,
+                    },
+                    headerShown: false,
+                }}
+            >
+                <Tabs.Screen
+                    name="index"
+                    options={{
+                        title: "shop",
+                        tabBarIcon(props) {
+                            return (
+                                <TabBarIcon {...props} name="shopping-cart" />
+                            );
+                        },
+                    }}
+                />
+                <Tabs.Screen
+                    name="orders"
+                    options={{
+                        title: "Orders",
+                        tabBarIcon(props) {
+                            return <TabBarIcon {...props} name="book" />;
+                        },
+                    }}
+                />
+            </Tabs>
+        </SafeAreaView>
+    );
+};
 
-// export default TabsLayout;
+export default TabsLayout;
 
 // const styles = StyleSheet.create({
 //     safeArea: {
