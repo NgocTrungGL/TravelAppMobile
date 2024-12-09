00cc8369a7edbba70b7a157a94fd284561b6f84b
ThuPham195 <pthu1951976@gmail.com>
2024-12-09 11:24:17 +0700

    add product/controller

--- src/app/product/slug.tsx
+++ src/app/product/slug.tsx
@@ -14,61 +14,61 @@ import { useCartStore } from "../../store/cart-store";
 import { getProduct } from "../../api/api";
 import { ActivityIndicator } from "react-native";
 
-// const ProductDetails = () => {
-//     const { slug } = useLocalSearchParams<{ slug: string }>();
-//     const toast = useToast();
+const ProductDetails = () => {
+    const { slug } = useLocalSearchParams<{ slug: string }>();
+    const toast = useToast();
 
-//     const { data: product, error, isLoading } = getProduct(slug);
+    const { data: product, error, isLoading } = getProduct(slug);
 
-//     const { items, addItem, incrementItem, decrementItem } = useCartStore();
+    const { items, addItem, incrementItem, decrementItem } = useCartStore();
 
-//     const cartItem = items.find((item) => item.id === product?.id);
+    const cartItem = items.find((item) => item.id === product?.id);
 
-//     const initialQuantity = cartItem ? cartItem.quantity : 0;
+    const initialQuantity = cartItem ? cartItem.quantity : 0;
 
-//     const [quantity, setQuantity] = useState(initialQuantity);
+    const [quantity, setQuantity] = useState(initialQuantity);
 
-//     if (isLoading) return <ActivityIndicator />;
-//     if (error) return <Text>Error: {error.message}</Text>;
-//     if (!product) return <Redirect href="/404" />;
+    if (isLoading) return <ActivityIndicator />;
+    if (error) return <Text>Error: {error.message}</Text>;
+    if (!product) return <Redirect href="/404" />;
 
-//     const increaseQuantity = () => {
-//         if (quantity < product.maxQuantity) {
-//             setQuantity((prev) => prev + 1);
-//             incrementItem(product.id);
-//         } else {
-//             toast.show("Cannot add more than maximum quantity", {
-//                 type: "warning",
-//                 placement: "top",
-//                 duration: 1500,
-//             });
-//         }
-//     };
+    const increaseQuantity = () => {
+        if (quantity < product.maxQuantity) {
+            setQuantity((prev) => prev + 1);
+            incrementItem(product.id);
+        } else {
+            toast.show("Cannot add more than maximum quantity", {
+                type: "warning",
+                placement: "top",
+                duration: 1500,
+            });
+        }
+    };
 
-//     const decreaseQuantity = () => {
-//         if (quantity > 1) {
-//             setQuantity((prev) => prev - 1);
-//             decrementItem(product.id);
-//         }
-//     };
+    const decreaseQuantity = () => {
+        if (quantity > 1) {
+            setQuantity((prev) => prev - 1);
+            decrementItem(product.id);
+        }
+    };
 
-//     const addToCart = () => {
-//         addItem({
-//             id: product.id,
-//             title: product.title,
-//             heroImage: product.heroImage,
-//             price: product.price,
-//             quantity,
-//             maxQuantity: product.maxQuantity,
-//         });
-//         toast.show("Added to cart", {
-//             type: "success",
-//             placement: "top",
-//             duration: 1500,
-//         });
-//     };
+    const addToCart = () => {
+        addItem({
+            id: product.id,
+            title: product.title,
+            heroImage: product.heroImage,
+            price: product.price,
+            quantity,
+            maxQuantity: product.maxQuantity,
+        });
+        toast.show("Added to cart", {
+            type: "success",
+            placement: "top",
+            duration: 1500,
+        });
+    };
 
-//     const totalPrice = (product.price * quantity).toFixed(2);
+    const totalPrice = (product.price * quantity).toFixed(2);
 
 //     return (
 //         <View style={styles.container}>
