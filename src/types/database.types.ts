2b472035132f398c30ad7e8312cd03fb338b363a
ThuPham195 <pthu1951976@gmail.com>
2024-12-09 11:48:53 +0700

    add database/tableupdate

--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -269,38 +269,38 @@ export type TablesInsert<
         : never
     : never;
 
-// export type TablesUpdate<
-//     PublicTableNameOrOptions extends
-//         | keyof PublicSchema["Tables"]
-//         | { schema: keyof Database },
-//     TableName extends PublicTableNameOrOptions extends {
-//         schema: keyof Database;
-//     }
-//         ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-//         : never = never
-// > = PublicTableNameOrOptions extends { schema: keyof Database }
-//     ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-//           Update: infer U;
-//       }
-//         ? U
-//         : never
-//     : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-//     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-//           Update: infer U;
-//       }
-//         ? U
-//         : never
-//     : never;
+export type TablesUpdate<
+    PublicTableNameOrOptions extends
+        | keyof PublicSchema["Tables"]
+        | { schema: keyof Database },
+    TableName extends PublicTableNameOrOptions extends {
+        schema: keyof Database;
+    }
+        ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+        : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+    ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+          Update: infer U;
+      }
+        ? U
+        : never
+    : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+          Update: infer U;
+      }
+        ? U
+        : never
+    : never;
 
-// export type Enums<
-//     PublicEnumNameOrOptions extends
-//         | keyof PublicSchema["Enums"]
-//         | { schema: keyof Database },
-//     EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-//         ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-//         : never = never
-// > = PublicEnumNameOrOptions extends { schema: keyof Database }
-//     ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-//     : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-//     ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-//     : never;
+export type Enums<
+    PublicEnumNameOrOptions extends
+        | keyof PublicSchema["Enums"]
+        | { schema: keyof Database },
+    EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+        ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+        : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+    : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never;
