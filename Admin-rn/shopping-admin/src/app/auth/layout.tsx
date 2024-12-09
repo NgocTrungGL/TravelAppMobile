2f15138346c855ba7677fa5db691e6029e902d88
NttP0607 <thanhphuoqq2004@gmail.com>
2024-12-09 14:57:11 +0700

    add layout

---
+++ Admin-rn/shopping-admin/src/app/auth/layout.tsx
@@ -0,0 +1,71 @@
+// import { ADMIN } from "@/constants/contants";
+// import { createClient } from "@/supabase/server";
+// import { redirect } from "next/navigation";
+// import { ReactNode } from "react";
+
+// export default async function AuthLayout({
+//     children,
+// }: Readonly<{
+//     children: ReactNode;
+// }>) {
+//     const supabase = createClient();
+
+//     const { data: authData } = await (await supabase).auth.getUser();
+
+//     if (authData?.user) {
+//         const { data, error } = await (await supabase)
+//             .from("users")
+//             .select("*")
+//             .eq("id", authData.user.id)
+//             .single();
+
+//         if (error || !data) {
+//             console.log("Error fetching user data", error);
+//             return;
+//         }
+
+//         if (data.type === ADMIN) return redirect("/admin");
+//     }
+
+//     return <>{children}</>;
+// }
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
+import { RenderMounted } from "@/components/render-mounted";
+import { ADMIN } from "@/constants/contants";
+import { createClient } from "@/supabase/server";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+
+export default async function AdminLayout({
+    children,
+}: Readonly<{
+    children: ReactNode;
+}>) {
+    const supabase = await createClient();
+
+    const { data: authData } = await supabase.auth.getUser();
+
+    if (authData?.user) {
+        const { data, error } = await supabase
+            .from("users")
+            .select("*")
+            .eq("id", authData.user.id)
+            .single();
+
+        if (error || !data) {
+            console.log("Error fetching user data", error);
+            return;
+        }
+
+        if (data.type === ADMIN) return redirect("/");
+    }
+
+    return (
+        <RenderMounted>
+            <Header />
+            <main className="min-h-[calc(100svh-128px)] py-3">{children}</main>
+            <Footer />
+        </RenderMounted>
+    );
+}
