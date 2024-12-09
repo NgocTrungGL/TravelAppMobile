d06835e44a3ccbc66db655510d3d8f6713c85f35
NguyenDucMinhKhoi <123210036@sv1.dut.udn.vn>
2024-12-09 10:41:22 +0700

    add page

--- src/app/(shop)/order/index.tsx
+++ src/app/(shop)/order/index.tsx
@@ -13,66 +13,66 @@ import { format } from "date-fns";
 import { Tables } from "../../../types/database.types";
 import { getMyOrders } from "../../../api/api";
 
-// const renderItem: ListRenderItem<Tables<"order">> = ({ item }) => (
-//     <Link href={`/orders/${item.slug}`} asChild>
-//         <Pressable style={styles.orderContainer}>
-//             <View style={styles.orderContent}>
-//                 <View style={styles.orderDetailsContainer}>
-//                     <Text style={styles.orderItem}>{item.slug}</Text>
-//                     <Text style={styles.orderDetails}>{item.description}</Text>
-//                     <Text style={styles.orderDate}>
-//                         {format(new Date(item.created_at), "MMM dd, yyyy")}
-//                     </Text>
-//                 </View>
-//                 <View
-//                     style={[
-//                         styles.statusBadge,
-//                         styles[`statusBadge_${item.status}`],
-//                     ]}
-//                 >
-//                     <Text style={styles.statusText}>
-//                         {item.status.toUpperCase()}
-//                     </Text>
-//                 </View>
-//             </View>
-//         </Pressable>
-//     </Link>
-// );
+const renderItem: ListRenderItem<Tables<"order">> = ({ item }) => (
+    <Link href={`/orders/${item.slug}`} asChild>
+        <Pressable style={styles.orderContainer}>
+            <View style={styles.orderContent}>
+                <View style={styles.orderDetailsContainer}>
+                    <Text style={styles.orderItem}>{item.slug}</Text>
+                    <Text style={styles.orderDetails}>{item.description}</Text>
+                    <Text style={styles.orderDate}>
+                        {format(new Date(item.created_at), "MMM dd, yyyy")}
+                    </Text>
+                </View>
+                <View
+                    style={[
+                        styles.statusBadge,
+                        styles[`statusBadge_${item.status}`],
+                    ]}
+                >
+                    <Text style={styles.statusText}>
+                        {item.status.toUpperCase()}
+                    </Text>
+                </View>
+            </View>
+        </Pressable>
+    </Link>
+);
 
-// const Orders = () => {
-//     const { data: orders, error, isLoading } = getMyOrders();
+const Orders = () => {
+    const { data: orders, error, isLoading } = getMyOrders();
 
-//     if (isLoading) return <ActivityIndicator />;
+    if (isLoading) return <ActivityIndicator />;
 
-//     if (error || !orders) return <Text>Error: {error?.message}</Text>;
+    if (error || !orders) return <Text>Error: {error?.message}</Text>;
 
-//     if (!orders.length)
-//         return (
-//             <Text
-//                 style={{
-//                     fontSize: 16,
-//                     color: "#555",
-//                     textAlign: "center",
-//                     padding: 10,
-//                 }}
-//             >
-//                 No orders created yet
-//             </Text>
-//         );
+    if (!orders.length)
+        return (
+            <Text
+                style={{
+                    fontSize: 16,
+                    color: "#555",
+                    textAlign: "center",
+                    padding: 10,
+                }}
+            >
+                No orders created yet
+            </Text>
+        );
 
-//     return (
-//         <View style={styles.container}>
-//             <Stack.Screen options={{ title: "Orders" }} />
-//             <FlatList
-//                 data={orders}
-//                 keyExtractor={(item) => item.id.toString()}
-//                 renderItem={renderItem}
-//             />
-//         </View>
-//     );
-// };
+    return (
+        <View style={styles.container}>
+            <Stack.Screen options={{ title: "Orders" }} />
+            <FlatList
+                data={orders}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={renderItem}
+            />
+        </View>
+    );
+};
 
-// export default Orders;
+export default Orders;
 
 // const styles: { [key: string]: any } = StyleSheet.create({
 //     container: {
