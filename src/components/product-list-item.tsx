19eef2d64270aa8e34a255a3a2ce4033809c927f
NguyenDucMinhKhoi <123210036@sv1.dut.udn.vn>
2024-12-09 10:32:28 +0700

    add page

--- src/components/product-list-item.tsx
+++ src/components/product-list-item.tsx
@@ -3,30 +3,30 @@ import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 import { Link } from "expo-router";
 import { Tables } from "../types/database.types";
 
-// export const ProductListItem = ({
-//     product,
-// }: {
-//     product: Tables<"product">;
-// }) => {
-//     return (
-//         <Link asChild href={`/product/${product.slug}`}>
-//             <Pressable style={styles.item}>
-//                 <View style={styles.itemImageContainer}>
-//                     <Image
-//                         source={{ uri: product.heroImage }}
-//                         style={styles.itemImage}
-//                     />
-//                 </View>
-//                 <View style={styles.itemTextContainer}>
-//                     <Text style={styles.itemTitle}>{product.title}</Text>
-//                     <Text style={styles.itemPrice}>
-//                         ${product.price.toFixed(2)}
-//                     </Text>
-//                 </View>
-//             </Pressable>
-//         </Link>
-//     );
-// };
+export const ProductListItem = ({
+    product,
+}: {
+    product: Tables<"product">;
+}) => {
+    return (
+        <Link asChild href={`/product/${product.slug}`}>
+            <Pressable style={styles.item}>
+                <View style={styles.itemImageContainer}>
+                    <Image
+                        source={{ uri: product.heroImage }}
+                        style={styles.itemImage}
+                    />
+                </View>
+                <View style={styles.itemTextContainer}>
+                    <Text style={styles.itemTitle}>{product.title}</Text>
+                    <Text style={styles.itemPrice}>
+                        ${product.price.toFixed(2)}
+                    </Text>
+                </View>
+            </Pressable>
+        </Link>
+    );
+};
 
 // const styles = StyleSheet.create({
 //     item: {
